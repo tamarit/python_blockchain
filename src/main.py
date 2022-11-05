c544d78ce018da36f46b2ac1fcb376d3042af100
tamarit <tamarit27@gmail.com>
2022-11-05 12:29:27 +0100

    Block added

--- src/main.py
+++ src/main.py
@@ -19,6 +19,10 @@ from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 
 
+###########################################
+# Classes
+###########################################
+
 class Client:
 
     def __init__(self):
@@ -57,6 +61,40 @@ class Transaction:
         h = SHA.new(str(self.to_dict()).encode('utf8'))
         return binascii.hexlify(signer.sign(h)).decode('ascii')
 
+class Block:
+
+    def __init__(self):
+        self.verified_transactions = []
+        self.previous_block_hash = ""
+        self.Nonce = ""
+
+class BlockChain:
+
+    def __init__(self):
+        self.TPCoins = []
+
+    def dump_blockchain (self):
+        print(f"Number of blocks in the chain: {len(self)}" + str(len (self)))
+        for x, block_temp in enumerate(self.TPCoins):
+            print(f"block # {x}")
+            for transaction in block_temp.verified_transactions:
+                display_transaction(transaction)
+                print ('-'*14)
+            print ('='*37)
+
+###########################################
+# Global variables
+###########################################
+
+last_block_hash = ""
+transactions = []
+TPCoins = []
+
+
+###########################################
+# Utility methods
+###########################################
+
 def print_with_separators(str_list):
     print(("\n" + "-"*5 + "\n").join(str_list))
 
@@ -69,12 +107,32 @@ def display_transaction(transaction):
         f"valtimeue: {transaction_dict['time']}",
     ])
 
-transactions = []
-
 def display_transactions(): 
     for transaction in transactions:
         display_transaction(transaction)
         print ('-'*14)
+
+def dump_blockchain():
+    print(f"Number of blocks in the chain: {len(TPCoins)}")
+    for x, block_temp in enumerate(TPCoins):
+        print(f"block # {x}")
+        for transaction in block_temp.verified_transactions:
+            display_transaction(transaction)
+            print ('-'*14)
+        print ('='*37)
+
+def sha256(message):
+    return hashlib.sha256(message.encode('ascii')).hexdigest()
+
+def mine(message, difficulty=1):
+    assert difficulty >= 1
+    prefix = '1'*difficulty
+    # for i in range(1000):
+    for i in range(10000000):
+        digest = sha256(str(hash(message)) + str(i))
+        if digest.startswith(prefix):
+            print(f"after {i} iterations found nonce: {digest}")
+            return digest
 
 if __name__ == '__main__':
     ###########################################
@@ -95,90 +153,122 @@ if __name__ == '__main__':
     # print (signature)
 
     ###########################################
-    # Test Multiple Transactions
-    Dinesh = Client()
-    Ramesh = Client()
-    Seema = Client()
-    Vijay = Client()
+    # # Test Multiple Transactions
+    # Dinesh = Client()
+    # Ramesh = Client()
+    # Seema = Client()
+    # Vijay = Client()
 
-    t1 = Transaction(
-        Dinesh,
-        Ramesh.identity,
-        15.0 
-    )
-    t1.sign_transaction()
-    transactions.append(t1)
+    # t1 = Transaction(
+    #     Dinesh,
+    #     Ramesh.identity,
+    #     15.0 
+    # )
+    # t1.sign_transaction()
+    # transactions.append(t1)
 
-    t2 = Transaction(
-        Dinesh,
-        Seema.identity,
-        6.0
-    )
-    t2.sign_transaction()
-    transactions.append(t2)
+    # t2 = Transaction(
+    #     Dinesh,
+    #     Seema.identity,
+    #     6.0
+    # )
+    # t2.sign_transaction()
+    # transactions.append(t2)
 
-    t3 = Transaction(
-        Ramesh,
-        Vijay.identity,
-        2.0 
-    )
-    t3.sign_transaction()
-    transactions.append(t3)
+    # t3 = Transaction(
+    #     Ramesh,
+    #     Vijay.identity,
+    #     2.0 
+    # )
+    # t3.sign_transaction()
+    # transactions.append(t3)
 
-    t4 = Transaction(
-        Seema,
-        Ramesh.identity,
-        4.0
-    )
-    t4.sign_transaction()
-    transactions.append(t4)
+    # t4 = Transaction(
+    #     Seema,
+    #     Ramesh.identity,
+    #     4.0
+    # )
+    # t4.sign_transaction()
+    # transactions.append(t4)
 
-    t5 = Transaction(
-        Vijay,
-        Seema.identity,
-        7.0
-    )
-    t5.sign_transaction()
-    transactions.append(t5)
+    # t5 = Transaction(
+    #     Vijay,
+    #     Seema.identity,
+    #     7.0
+    # )
+    # t5.sign_transaction()
+    # transactions.append(t5)
 
-    t6 = Transaction(
-        Ramesh,
-        Seema.identity,
-        3.0
-    )
-    t6.sign_transaction()
-    transactions.append(t6)
+    # t6 = Transaction(
+    #     Ramesh,
+    #     Seema.identity,
+    #     3.0
+    # )
+    # t6.sign_transaction()
+    # transactions.append(t6)
 
-    t7 = Transaction(
-        Seema,
-        Dinesh.identity,
-        8.0
-    )
-    t7.sign_transaction()
-    transactions.append(t7)
+    # t7 = Transaction(
+    #     Seema,
+    #     Dinesh.identity,
+    #     8.0
+    # )
+    # t7.sign_transaction()
+    # transactions.append(t7)
 
-    t8 = Transaction(
-        Seema,
-        Ramesh.identity,
-        1.0
-    )
-    t8.sign_transaction()
-    transactions.append(t8)
+    # t8 = Transaction(
+    #     Seema,
+    #     Ramesh.identity,
+    #     1.0
+    # )
+    # t8.sign_transaction()
+    # transactions.append(t8)
 
-    t9 = Transaction(
-        Vijay,
-        Dinesh.identity,
-        5.0
-    )
-    t9.sign_transaction()
-    transactions.append(t9)
+    # t9 = Transaction(
+    #     Vijay,
+    #     Dinesh.identity,
+    #     5.0
+    # )
+    # t9.sign_transaction()
+    # transactions.append(t9)
 
-    t10 = Transaction(
-        Vijay,
-        Ramesh.identity,
-        3.0
-    )
-    t10.sign_transaction()
-    transactions.append(t10)
+    # t10 = Transaction(
+    #     Vijay,
+    #     Ramesh.identity,
+    #     3.0
+    # )
+    # t10.sign_transaction()
+    # transactions.append(t10)
  
-    display_transactions()
+    # display_transactions()
+
+    ###########################################
+    # # Test Block Creation & Visualization
+    # Dinesh = Client()
+    # t0 = Transaction(
+    #     "Genesis",
+    #     Dinesh.identity,
+    #     500.0 
+    # )
+    # block0 = Block()
+
+    # block0.previous_block_hash = None
+    # Nonce = None
+
+    # block0.verified_transactions.append(t0)
+
+    # digest = hash(block0)
+    # last_block_hash = digest
+    # print(last_block_hash)
+
+    # TPCoins.append(block0)
+    
+    # dump_blockchain()
+
+    ###########################################
+    # Mining test
+    
+    mine("test message daa dasd asda sd", 4)
+
+
+
+
